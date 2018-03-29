871fe0ab2a864aeefbfdecf28b8f7853c8192056
Daniel Santos <dfsantosbu@unal.edu.co>
2018-03-29 13:49:09 -0400

    Create basic procesor data, based on chosed columns.

--- launcher.py
+++ launcher.py
@@ -1,5 +1,34 @@
+from __future__ import print_function 
+import pandas as pa
+import numpy as nu
+import datetime as dt
+
+def readRelevantColumns():
+    aux = open("assets/relevant_cols", "r").read().split("\n")[:-1]
+    cols  = [int(y) for y in aux]
+    return cols
+
+def getRelevantData(namefile, codeSession, cols):
+    now = dt.datetime.now()
+    strnow = str(now.strftime("%Y-%m-%d"))
+    nameExportFile = "clean_data_{}.{}".format(strnow,"csv")
+    
+    data = pa.read_csv("data/"+namefile);
+    data = data[ data["session.code"] == codeSession ] 
+    data = data.iloc[:,cols]
+    data.to_csv(nameExportFile)
+
+    print( "The file with name  = {} was created at {}".format(nameExportFile, strnow) )
+    print( "This file contains the clean data")
 
 
-if __name__  == "__main__":
-    namefile = "file.csv"
+if __name__  == "__main__":   
+    namefile = "data.csv"
+    codeSession = "6h9k4r19"
+    labelSession = "1803231630C0106"
 
+    cols =   readRelevantColumns()
+    getRelevantData(namefile, codeSession, cols)
+    
+
+    
