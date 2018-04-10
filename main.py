750e70fc28eaf2585183ec55285efb20075a8fae
Daniel Santos <dfsantosbu@unal.edu.co>
2018-04-10 12:16:41 -0400

    Create good version for front end for upload the files.

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mys3cr31'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
 def allowed_file(filename):
   return '.' in filename and \
   filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
